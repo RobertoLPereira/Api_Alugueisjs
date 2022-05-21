0a145d8743fc36ab6824e03d123a593d4dd52c05
RobertoLPereira <roberto.lpereira65@gmail.com>
2022-05-21 19:30:10 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+import express from 'express';
+import consign from 'consign';
+var bodyParser = require('body-parser');
+
+const app = express();
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({extended: true}));
+// Routes
+consign({cwd: __dirname})
+  .include('libs/config.js')
+  .then('db.js')
+  .then('libs/middlewares.js')
+  .then('routes')
+  .then('libs/boot.js')
+  .then('controllers')
+  .into(app);
---
+++ src/libs/config_old.js
@@ -0,0 +1,27 @@
+module.exports = {
+  hostname: 'ec2-34-239-196-254.compute-1.amazonaws.com',
+  database: 'dftuon032e0g60',
+  username: 'gahtllmmerkajh',
+  password: 'a4bb531d36c4899a01cb1f884900d119ba711e4dc1bfd932e67c7ca75ad8c3bb',
+  params: {
+    dialect: 'postgres',
+    storage: 'ControleAlugueis-db.dftuon032e0g60',
+    define: {
+      underscored: true
+    },
+    operatorsAliases: false
+  }
+};
+/*
+  database: 'ControleAluguel',
+  username: 'postgres',
+  password: 'rlp562',
+  params: {
+    dialect: 'postgres',
+    storage: 'ControleAluguel-db.postgres',
+    define: {
+      underscored: true
+    },
+    operatorsAliases: false
+  }
+  */
